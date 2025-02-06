419780cc466bd01dac101080bf354eaa0f484424
Yeonwah <cute_run@naver.com>
2025-02-06 20:17:43 +0900

    [Chore] Player 구성 초기

--- TextRPGTeam30/ICharacter.cs
+++ TextRPGTeam30/ICharacter.cs
@@ -8,5 +8,50 @@ namespace TextRPGTeam30
 {
     internal interface ICharacter
     {
+        public int Level { get; set; }
+        public string Name { get; set; }
+
+        public int Hp { get; set; }
+
+        public int CritRate { get; set; }
+
+        public float Attack { get; set; }
+
+        public int CritDamage { get; set; }
+        public int Evasosion { get; set; }
+
+        public void TakeDamage(int damage)
+
+        {
+
+        }
+
+        public void Dead()
+        {
+
+
+
+        }
+
     }
+        
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
--- TextRPGTeam30/Player.cs
+++ TextRPGTeam30/Player.cs
@@ -1,12 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TextRPGTeam30
 {
-    internal class Player
+    internal class Player : ICharacter
+
     {
+
+
+        public int mp;
+
+        public int gold;
+
+        public int exp;
+
+        public lJob job;
+
+        public List<Equipable> equipment;
+
+        public List<Consumable> consumables;
+
+        public Weapon? equipWeapon;
+
+        public Armor? equipArmor;
+
+        public int Level { get; set; }
+
+        public string Name { get; set; }
+
+        public int Hp { get; set; }
+
+        public int CritRate { get; set; }
+
+        public float Attack { get; set; }
+
+        public int CritDamage { get; set; }
+        public int Evasosion { get; set; }
+
+
+        public Player()
+        {
+
+
+        }
+        public void TakeDamage(int damage)
+        { 
+        
+        
+        }
+
+        public void Dead()
+        {
+
+
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
