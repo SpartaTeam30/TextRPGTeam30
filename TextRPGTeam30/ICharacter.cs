fb510fc497668319a939373d38dafee77b414d54
KSangMin <fhfpszmfnwm@naver.com>
2025-02-06 20:26:47 +0900

    [Style] Player 코드 재정렬
    공백 제거, 안 쓰는 네임스페이스 using 제거

--- TextRPGTeam30/ICharacter.cs
+++ TextRPGTeam30/ICharacter.cs
@@ -1,27 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TextRPGTeam30
+﻿namespace TextRPGTeam30
 {
     internal interface ICharacter
     {
         public int Level { get; set; }
         public string Name { get; set; }
-
         public int Hp { get; set; }
-
         public int CritRate { get; set; }
-
         public float Attack { get; set; }
-
         public int CritDamage { get; set; }
         public int Evasosion { get; set; }
 
         public void TakeDamage(int damage)
-
         {
 
         }
@@ -29,29 +18,7 @@ namespace TextRPGTeam30
         public void Dead()
         {
 
-
-
         }
-
     }
-        
-
-       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
