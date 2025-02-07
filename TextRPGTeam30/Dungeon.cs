7b1d0befb95ec05601cfe0f627cdf7862c50bd19
royun0307 <ychul030737@gmail.com>
2025-02-07 14:40:31 +0900

    {Feat} Dungeon 생성자와 던전클리어 기능 구현
    Dungeon 생성자와 던전클리어 기능 구현(보상아이템 미구현)

--- TextRPGTeam30/Dungeon.cs
+++ TextRPGTeam30/Dungeon.cs
@@ -5,26 +5,36 @@ namespace TextRPGTeam30
 		public int stage;
 		public int rewardExp;
 		public int rewardGold;
-		List<Monster> monsters;
-		List<Item> items;
+		public List<Monster> monsters;
+		public List<Item> items;
 
 		public Dungeon(int _stage, List<Monster> _monsters)
 		{
 			stage = _stage;
-			monsters = _monsters;
-		}
-		public void StartDungeon()
-		{
+			rewardExp = 0;
+			monsters = new List<Monster>();
+			rewardGold = 0;
 
+			for (int i = 0; i < 3; i++)
+			{
+				Monster monster = _monsters[new Random().Next(0, _monsters.Count)];
+				monster.SetLevel(new Random().Next(stage / 5 + 1, stage / 5 + 6));
+				monsters.Add(monster);
+			}
 		}
 		public void DungeonSuccess()
 		{
-
-		}
+			foreach (Monster monster in monsters)
+			{
+				rewardExp += monster.Level;
+			}
+			rewardGold = new Random().Next(stage * 100, stage * 200);
+			//item
+        }
 
 		public void DungeonFail()
 		{
-
+			
 		}
 	}
 }
