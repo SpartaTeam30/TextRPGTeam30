c310c9b5b09a25ae06f621dcd2f9fea795cc1dc0
Yeonwah <cute_run@naver.com>
2025-02-07 11:44:39 +0900

    [Feat] 캐릭터 & 플레이어

--- TextRPGTeam30/Player.cs
+++ TextRPGTeam30/Player.cs
@@ -14,23 +14,48 @@
         public string Name { get; set; }
         public int Hp { get; set; }
         public int CritRate { get; set; }
-        public float Attack { get; set; }
-        public int CritDamage { get; set; }
-        public int Evasosion { get; set; }
-
-        public Player()
+        public Character(int critRate)
         {
+            CritRate = critRate;
+        }
+        public float Attack { get; set; }
+        public Character(float attack)
+        {
+            Attack = attack;
+        }
+        public int CritDamage { get; set; }
+        public int Evasion { get; set; }
 
+        public Player(string name, int level, int hp, int mp, int gold, int exp, Job job)
+        {
+            Name = name;
+            Level = level;
+            Hp = hp;
+            Mp = mp;
+            Gold = gold;
+            Exp = exp;
+            Job = job;
         }
 
         public void TakeDamage(int damage)
-        { 
-        
+        {
+            Hp -= damage;
+
+            if (Hp <= 0)
+            {
+                Hp = 0;
+                Dead();  // 체력이 0 이하일 시
+            }
+
         }
 
         public void Dead()
         {
-
+            Console.WriteLine($"{Name} 가 죽었습니다.");
         }
-    }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Level: {Level}, HP: {Hp}");
+        }
 }
