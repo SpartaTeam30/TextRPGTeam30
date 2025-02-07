88bda80e65da4de2e415e8ce433d94989ad72d49
Yeonwah <cute_run@naver.com>
2025-02-07 21:07:14 +0900

    [Style] 정리

---
+++ TextRPGTeam30/Armor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPGTeam30
+{
+    internal class Armor : Equipable
+    {
+        public int defense;
+
+        public void Toggle()
+        {
+
+        }
+                 // 인벤토리 
+    }
+}
---
+++ TextRPGTeam30/Equipable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPGTeam30
+{
+    internal class Equipable : IItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        // public int Price { get; set; }
+        public string IDescription { get; set; } // 변수 설명용
+
+        public bool isEquip;
+
+        public void Toggle(Player player)
+        {
+
+        }
+    }
+}
+
