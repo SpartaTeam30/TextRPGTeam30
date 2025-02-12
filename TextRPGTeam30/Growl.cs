f850922b271072562925ffd07ab8e7fb861d4a53
royun0307 <ychul030737@gmail.com>
2025-02-12 11:14:43 +0900

    [Feat] 유틸리티 스킬 사용시 변경 내용 출력
    유틸리티 스킬 사용시 변경 내용 출력

--- TextRPGTeam30/Growl.cs
+++ TextRPGTeam30/Growl.cs
@@ -2,9 +2,9 @@
 {
     public class Growl : UtilitySkill
     {
-        public string name = "울부짖기";
         public Growl()
         {
+            name = "울부짖기";
             cost = 15;
             dAttack = -1;
             count = 1;
@@ -13,6 +13,15 @@
         public Growl(int cost, int dAttack, int dDefense, int count) : base(cost, dAttack, dDefense, count)
         {
 
+        }
+
+        public override void PrintUseSkill(ICharacter target)
+        {
+            Console.WriteLine($"{name} 스킬 사용! (MP {cost} 소모)");
+
+            Console.WriteLine($" {target.Name}의 공격력 {dAttack}");
+            
+            return;
         }
     }
 }
